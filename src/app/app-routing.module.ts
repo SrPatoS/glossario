1ce8914eff493eec029765376de403d04d9efd18
Vinicius <vavizvini000@gmail.com>
2024-12-06 17:10:05 -0300

    ajusta menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 
 const routes: Routes = [
-  {
-    path: "glossario",
-    component: DashboardPageComponent
-  },
-  {
-    path: "guia",
-    component: DashboardPageComponent
-  },
-  {
-    path: "agradecimentos",
-    component: DashboardPageComponent
-  }
+  { path: 'glossario', component: DashboardPageComponent },
+  { path: 'guia', component: DashboardPageComponent },
+  { path: 'agradecimentos', component: DashboardPageComponent },
+  { path: '', redirectTo: '/glossario', pathMatch: 'full' }, // Redirecionamento padrão
+  { path: '**', redirectTo: '/glossario' }, // Redirecionamento para rota inválida
 ];
 
 @NgModule({
