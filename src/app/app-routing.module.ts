b2ffa8d7a1c5a3ea2e09cd56d274e4d7ae53f9bf
Vinicius <vavizvini000@gmail.com>
2024-12-08 01:22:13 -0300

    finaliza base do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardPageComponent } from './pages/dashboard-page/dashboard-page.component';
 import { GlossaryPageComponent } from './pages/glossary-page/glossary-page.component';
 import { GlossaryDetailPageComponent } from './pages/glossary-detail-page/glossary-detail-page.component';
+import { GuidePageComponent } from './pages/guide-page/guide-page.component';
+import { CreditsPageComponent } from './pages/credits-page/credits-page.component';
+import { ThanksPageComponent } from './pages/thanks-page/thanks-page.component';
 
 const routes: Routes = [
+  { path: 'inicio', component: DashboardPageComponent },
   { path: 'glossario', component: GlossaryPageComponent },
   { path: 'glossario/detalhe/:id', component: GlossaryDetailPageComponent },
-  { path: 'guia', component: DashboardPageComponent },
-  { path: 'agradecimentos', component: DashboardPageComponent },
-  { path: '', redirectTo: '/glossario', pathMatch: 'full' }, // Redirecionamento padrão
-  { path: '**', redirectTo: '/glossario' }, // Redirecionamento para rota inválida
+  { path: 'guia', component: GuidePageComponent },
+  { path: 'creditos', component: CreditsPageComponent },
+  { path: 'agradecimentos', component: ThanksPageComponent },
+  { path: '', redirectTo: '/glossario', pathMatch: 'full' },
+  { path: '**', redirectTo: '/glossario' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
