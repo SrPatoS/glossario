fd2065a2a8c5bcac28853d20aa64772c9e25dbca
Vinicius <vavizvini000@gmail.com>
2024-12-08 01:36:59 -0300

    adiciona busca

--- src/app/pages/glossary-page/glossary-page.component.ts
+++ src/app/pages/glossary-page/glossary-page.component.ts
@@ -5,18 +5,34 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-glossary-page',
   standalone: false,
-
   templateUrl: './glossary-page.component.html',
   styleUrl: './glossary-page.component.scss'
 })
 export class GlossaryPageComponent {
   data = fakeData;
+  dataSearch?: IGlossaryData[];
 
   constructor(private router: Router) {
   }
 
+  ngOnInit() {
+    this.dataSearch = this.data;
+  }
 
   openModal(item: IGlossaryData) {
     this.router.navigate(['/glossario/detalhe', item.id]);
+  }
+
+  search(query: Event) {
+    const newValue = (query.target as HTMLInputElement).value;
+
+    if (!newValue.length) {
+      this.dataSearch = this.data;
+      return;
+    }
+
+    this.dataSearch = this.data.filter(item =>
+      item.title.toLowerCase().includes(newValue)
+    );
   }
 }
